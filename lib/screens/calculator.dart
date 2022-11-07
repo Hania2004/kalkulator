c86684fed1e7a66a4b91ada9661e0c2dbbd3e875
hania2004 <haniagoliszek@wp.pl>
2022-11-07 14:39:19 +0100

    init

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold
+      (
+      body: Center(child: Text('123456'),),
+    );
+  }
+}
+
