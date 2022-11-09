7406d4a7d5177d50aabbba6ac06e7de8494cd4d6
hania2004 <haniagoliszek@wp.pl>
2022-11-09 08:13:41 +0100

    init

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -5,10 +5,74 @@ class Calculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold
-      (
-      body: Center(child: Text('123456'),),
-    );
+    return Scaffold(
+      backgroundColor: Colors.indigoAccent,
+      appBar: AppBar(centerTitle: true, title: Text('Calculator'),backgroundColor: Colors.grey),
+        body: Padding(padding: EdgeInsets.all(10),
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('5+5', style: TextStyle(fontSize: 60),),
+                Padding(padding: EdgeInsets.all(6)),
+                Text('10', style: TextStyle(fontSize: 60),),
+                Padding(padding: EdgeInsets.all(6)),
+                Text('', style: TextStyle(fontSize: 80),),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('7', style: TextStyle(fontSize: 70)),
+                    Text('8', style: TextStyle(fontSize: 70)),
+                    Text('9', style: TextStyle(fontSize: 70)),
+                    Text('+', style: TextStyle(fontSize: 70)),
+                  ],
+                ),    Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('4', style: TextStyle(fontSize: 70)),
+                    Text('5', style: TextStyle(fontSize: 70)),
+                    Text('6', style: TextStyle(fontSize: 70)),
+                    Text('-', style: TextStyle(fontSize: 70)),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('C', style: TextStyle(fontSize: 70)),
+                    Text('0', style: TextStyle(fontSize: 70)),
+                    Text('=', style: TextStyle(fontSize: 70)),
+                    Text('/', style: TextStyle(fontSize: 70)),
+                  ],
+                ),
+
+              ],
+
+         )
+        )
+      );
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//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
