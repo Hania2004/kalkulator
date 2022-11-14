3165b6f97ceb3ab8f72338d7a9b8bf323889a99d
Hania2004 <117639562+Hania2004@users.noreply.github.com>
2022-11-14 12:57:34 +0100

    Update calculator.dart

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -56,7 +56,7 @@ class _CalculatorState extends State<Calculator> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                numButton("C", Colors.grey, Colors.black),
+                numButton("AC", Colors.grey, Colors.black),
                 numButton("+/-", Colors.grey, Colors.black),
                 numButton("%", Colors.grey, Colors.black),
                 numButton("/", Colors.grey, Colors.black),
@@ -123,45 +123,120 @@ class _CalculatorState extends State<Calculator> {
     );
   }
 
-  int firstNumber = 0;
-  int secondNumber = 0;
-  String result = "";
-  String text = "";
-  String operation = "";
+  dynamic text ='0';
+  double numOne = 0;
+  double numTwo = 0;
 
-  void calculate(String btnText) {
-    if (btnText == "C") {
-      result = "";
-      text = "";
-      firstNumber = 0;
-      secondNumber = 0;
-    } else if (btnText == "+" ||
-        btnText == "-" ||
-        btnText == "x" ||
-        btnText == "/") {
-      firstNumber = int.parse(text);
-      result = "";
-      operation = btnText;
-    } else if (btnText == "=") {
-      secondNumber = int.parse(text);
-      if (operation == "+") {
-        result = (firstNumber + secondNumber).toString();
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic opr = '';
+  dynamic preOpr = '';
+  void calculate(btnText) {
+
+
+    if(btnText  == 'AC') {
+      text ='0';
+      numOne = 0;
+      numTwo = 0;
+      result = '';
+      finalResult = '0';
+      opr = '';
+      preOpr = '';
+
+    } else if( opr == '=' && btnText == '=') {
+
+      if(preOpr == '+') {
+        finalResult = add();
+      } else if( preOpr == '-') {
+        finalResult = sub();
+      } else if( preOpr == 'x') {
+        finalResult = mul();
+      } else if( preOpr == '/') {
+        finalResult = div();
       }
-      if (operation == "-") {
-        result = (firstNumber - secondNumber).toString();
+
+    } else if(btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' || btnText == '=') {
+
+      if(numOne == 0) {
+        numOne = double.parse(result);
+      } else {
+        numTwo = double.parse(result);
       }
-      if (operation == "x") {
-        result = (firstNumber * secondNumber).toString();
+
+      if(opr == '+') {
+        finalResult = add();
+      } else if( opr == '-') {
+        finalResult = sub();
+      } else if( opr == 'x') {
+        finalResult = mul();
+      } else if( opr == '/') {
+        finalResult = div();
       }
-      if (operation == "/") {
-        result = (firstNumber ~/ secondNumber).toString();
-      }
-    } else {
-      result = int.parse(text + btnText).toString();
+      preOpr = opr;
+      opr = btnText;
+      result = '';
     }
-    setState(() {
-      text = result;
-    });
-  }
-}
+    else if(btnText == '%') {
+      result = numOne / 100;
+      finalResult = doesContainDecimal(result);
+    } else if(btnText == '.') {
+      if(!result.toString().contains('.')) {
+        result = '$result.';
+      }
+      finalResult = result;
+    }
 
+    else if(btnText == '+/-') {
+      result.toString().startsWith('-') ? result = result.toString().substring(1): result = '-$result';
+      finalResult = result;
+
+    }
+
+    else {
+      result = result + btnText;
+      finalResult = result;
+    }
+
+
+    setState(() {
+      text = finalResult;
+    });
+
+  }
+
+
+  String add() {
+    result = (numOne + numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (numOne - numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String mul() {
+    result = (numOne * numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String div() {
+    result = (numOne / numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+
+  String doesContainDecimal(dynamic result) {
+
+    if(result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if(!(int.parse(splitDecimal[1]) > 0)) {
+        return result = splitDecimal[0].toString();
+      }
+    }
+    return result;
+  }
+
+}
